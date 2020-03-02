e7588b12a12a11d0346247fe70f4230fffa4bf4e
Héctor Sevilla <h2d_07@hotmail.com>
2020-03-02 15:02:25 -0600

    adding partial for favicons

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+	<?php get_template_part( 'partials/favicon' ); ?>
+
     <?php if(is_archive()):
 
      //Obtener post type
